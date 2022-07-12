44a44a4080567d0840b3b663b1f83a1aae78f5d8
Ritik Roshan Sahu <rsahu0602@gmail.com>
2022-07-12 22:28:42 +0530

    styled the entire app

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,11 @@ const App = () => {
   const { winner, winningSquares } = calculateWinner(current.board);
 
   const handleSquareClick = position => {
-    if (current.board[position] || winner) {
+    if (
+      current.board[position] ||
+      winner ||
+      currentMove + 1 !== history.length
+    ) {
       return;
     }
 
@@ -48,17 +52,25 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>TIC TAC TOE</h1>
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
       <StatusMessage winner={winner} current={current} />
       <Board
         board={current.board}
         handleSquareClick={handleSquareClick}
         winningSquares={winningSquares}
       />
-      <button type="button" onClick={onNewGame}>
+      <button
+        type="button"
+        onClick={onNewGame}
+        className={`btn-reset ${winner ? 'active' : ''}`}
+      >
         Start new game
       </button>
+      <h2 style={{ fontWeight: 'normal' }}>Current game history</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className="bg-balls" />
     </div>
   );
 };
